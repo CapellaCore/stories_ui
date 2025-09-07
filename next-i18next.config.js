d1468fd168249e1d6e490c3e376b7b5ca86f68f7
Raman Karpiak <roman.karpiak@gmail.com>
2025-09-07 14:04:53 +0200

    fix deployment bug

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
 // next-i18next.config.js
+const path = require('path');
+
 module.exports = {
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'pl'],
-    },
-    reloadOnPrerender: process.env.NODE_ENV === 'development',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'pl'],
+  },
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+  localePath: path.resolve('./public/locales'),
 };
